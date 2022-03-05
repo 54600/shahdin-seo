c085cbcb88d9f661b78fb58637242b1ff6590c44
MOHSINALI5460 <mohsin.ali@kics.edu.pk>
2022-03-05 12:16:33 +0500

    main file change

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ def spin():
     return jsonify(r)
  
 if __name__=="__main__":
-    app.run(debug=True,host="192.168.56.1",port=5000)
+    app.run(debug=True)
+    # app.run(debug=True,host="192.168.56.1",port=5000)
     
     #app.run(host="192.168.56.1",port=5000)
